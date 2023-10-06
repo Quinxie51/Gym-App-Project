15118090cbc4399eb5d9f21b89d2f7fbc36fa6d5
Cody <codybyrd21@augustana.edu>
2023-10-05 23:44:05 -0500

    Added a sidebar to home screen with 6 buttons; New Lesson, folder, deleted, favorites, q and a, and settings. I also added a hbox to the top of the home page

--- IbisGymProject/src/main/java/edu/augustana/PrimaryController.java
+++ IbisGymProject/src/main/java/edu/augustana/PrimaryController.java
@@ -2,8 +2,23 @@ package edu.augustana;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PrimaryController {
+
+    @FXML private Button deletedButton;
+
+    @FXML private Button favoritesButton;
+
+    @FXML private Button folderButton;
+
+    @FXML private Button newLessonButton;
+
+    @FXML private Button primaryButton;
+
+    @FXML private Button qaButton;
+
+    @FXML private Button settingsButton;
 
     @FXML
     private void switchToSecondary() throws IOException {
