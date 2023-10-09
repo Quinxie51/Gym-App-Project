ad9fe2b727c334d353147d5661b55bb74761d80d
Cody <codybyrd21@augustana.edu>
2023-10-09 16:06:38 -0500

    Renamed files to better suit their roles, also created a page for creating a new lesson and begining work on making that new page a popup on the home page

--- IbisGymProject/src/main/java/edu/augustana/MainApp.java
+++ IbisGymProject/src/main/java/edu/augustana/MainApp.java
@@ -11,13 +11,13 @@ import java.io.IOException;
 /**
  * JavaFX App
  */
-public class App extends Application {
+public class MainApp extends Application {
 
     private static Scene scene;
     //HELLO THUNDERDOMEApp
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("mainHomepage"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
@@ -27,7 +27,7 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
---
+++ IbisGymProject/src/main/java/edu/augustana/MainHomepageController.java
@@ -0,0 +1,45 @@
+package edu.augustana;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+public class MainHomepageController {
+
+    // for mainHomepage.fxml
+    @FXML private Button deletedButton;
+    @FXML private Button favoritesButton;
+    @FXML private Button folderButton;
+    @FXML private Button newLessonButton;
+    @FXML private Button qaButton;
+    @FXML private Button settingsButton;
+
+    //for newLessonPopup.fxml
+    @FXML private Button doneMakingLesson;
+    @FXML private CheckBox folderCheckbox;
+    @FXML private TextField folderText;
+    @FXML private TextField newLessonText;
+
+    @FXML
+    private void initialize() {
+        //newLessonText.setOnKeyTyped(e -> handleQuestTextChange(e));
+    }
+
+    @FXML
+    private void openFolderView() throws IOException {
+        MainApp.setRoot("secondary");
+    }
+
+    @FXML
+    private void backToHome() throws IOException {
+        MainApp.setRoot("mainHomepage");
+    }
+
+    @FXML
+    private void handleCreateNewLesson() throws IOException {
+        MainApp.setRoot("newLessonPopup");
+    }
+}
