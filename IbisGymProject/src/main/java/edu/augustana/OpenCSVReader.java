9a1164e9e0110bf435995ab2d1e000d331732623
Cami <camiflores21@augustana.edu>
2023-10-15 13:58:19 -0500

    added an OpenCSVReader class and modified the module.java to read it

---
+++ IbisGymProject/src/main/java/edu/augustana/OpenCSVReader.java
@@ -0,0 +1,9 @@
+package edu.augustana;
+import com.opencsv.CSVReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class OpenCSVReader {
+}
