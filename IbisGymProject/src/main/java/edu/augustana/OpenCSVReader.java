71032dde9253f4177d6dc5264ae2862f193b1411
Cami <camiflores21@augustana.edu>
2023-10-15 14:23:29 -0500

    working on OpenCSVReader to get indexes to match up

--- IbisGymProject/src/main/java/edu/augustana/OpenCSVReader.java
+++ IbisGymProject/src/main/java/edu/augustana/OpenCSVReader.java
@@ -4,6 +4,36 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
 
 public class OpenCSVReader {
+    private static final String CSV_FILE_PATH = "src/main/resources/edu/augustana/DEMO1.csv";
+
+    public static void main(String[] args) throws IOException, CsvValidationException {
+
+        try (
+                Reader reader = Files.newBufferedReader(Paths.get(CSV_FILE_PATH));
+                CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1).build();
+        ) {
+            // Reading Cards One by One in a String array
+            String[] nextCard;
+            while ((nextCard = csvReader.readNext()) != null) {
+                System.out.println("CODE : " + nextCard[0]);
+                System.out.println("Event : " + nextCard[1]);
+                System.out.println("Category : " + nextCard[2]);
+                System.out.println("Title : " + nextCard[3]);
+                System.out.println("Pack : " + nextCard[4]);
+                System.out.println("Folder : " + nextCard[5]);
+                System.out.println("Image : " + nextCard[6]);
+                System.out.println("Gender : " + nextCard[7]);
+                System.out.println("Model : " + nextCard[8]);
+                System.out.println("Sex : " + nextCard[9]);
+                System.out.println("Level : " + nextCard[10]);
+                System.out.println("Equipment : " + nextCard[11]);
+                System.out.println("Keywords : " + nextCard[12]);
+                System.out.println("==========================");
+            }
+        }
+    }
 }
