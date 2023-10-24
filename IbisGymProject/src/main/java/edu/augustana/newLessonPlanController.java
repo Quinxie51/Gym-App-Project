8f7276306a716825638cb3c7537c5526c0dfd4aa
Quinxie51 <114624411+Quinxie51@users.noreply.github.com>
2023-10-24 12:45:55 -0500

    "add drag and drop function"

--- IbisGymProject/src/main/java/edu/augustana/newLessonPlanController.java
+++ IbisGymProject/src/main/java/edu/augustana/newLessonPlanController.java
@@ -1,8 +1,17 @@
 package edu.augustana;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.TransferMode;
+
+
+
+
 
 public class newLessonPlanController {
     //datafields
@@ -12,12 +21,25 @@ public class newLessonPlanController {
     private void switchToPrimary() throws IOException {
         MainApp.setRoot("mainHomepage");
     }
+    @FXML
+    private ImageView imageView;
 
 
-     public void setLessonPlanName(String text) {
+
+
+
+    public void setLessonPlanName(String text) {
         lessonPlanName.setText(text);
     }
-
-
+    public void handleDrop(DragEvent event){
+        //List<File> files = event.getDragboard().getFiles();
+        //Image img = new Image(new FileInputStream(files.get(0)));
+        //imageView.setImage(img);
+        }
+    public void handleDragOver(DragEvent event) {
+        if (event.getDragboard().hasFiles()) {
+            event.acceptTransferModes(TransferMode.ANY);
+        }
+    }
 
 }
