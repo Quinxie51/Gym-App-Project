1af3b35f647f24755848171664f7de3222dd94c2
Cami <camiflores21@augustana.edu>
2023-11-03 18:24:23 -0500

    Added a HashMap to filter the Categories and added a SetCategoryMap method

--- IbisGymProject/src/main/java/edu/augustana/data/CategoryFilter.java
+++ IbisGymProject/src/main/java/edu/augustana/data/CategoryFilter.java
@@ -1,14 +1,29 @@
 package edu.augustana.data;
 
 import java.util.Objects;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CategoryFilter implements CardFilter{
 
     private final String selectedCategory;
+    HashMap<String, ArrayList<String>> categoryMap = new HashMap<>();
 
     public CategoryFilter(String selectedOption) {
         this.selectedCategory = selectedOption;
     }
+
+    void setCategoryMap(String key, String value) {
+        if (categoryMap.containsKey(key)) {
+            categoryMap.get(key).add(value);
+        } else {
+            ArrayList<String> values = new ArrayList<>();
+            values.add(value);
+            categoryMap.put(key, values);
+        }
+    }
+
+
     public boolean matches (Card potentialMatchCard) {
         return Objects.equals(potentialMatchCard.getCategory(), selectedCategory);
     }
