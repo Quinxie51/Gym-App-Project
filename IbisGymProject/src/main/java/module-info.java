15c25d67ac729e962b2053ac4f089194c51f14e2
Cody <codybyrd21@augustana.edu>
2023-11-21 12:28:30 -0600

    Solved issue in module info where app wouldnt run.

--- IbisGymProject/src/main/java/module-info.java
+++ IbisGymProject/src/main/java/module-info.java
@@ -3,8 +3,7 @@ module edu.augustana {
     requires javafx.fxml;
     requires com.opencsv;
     requires com.google.gson;
-
-    opens edu.augustana to javafx.fxml;
+    
     exports edu.augustana.data;
     opens edu.augustana.data to javafx.fxml, com.google.gson;
     exports edu.augustana.ui;
