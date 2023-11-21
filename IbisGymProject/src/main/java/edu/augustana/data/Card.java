a28eb734075fe4703deb919753912dabd5b90d4e
Cody <codybyrd21@augustana.edu>
2023-11-20 19:05:35 -0600

    Fixed issue where certain dragged cards would not appear, now all dragged cards appear.

--- IbisGymProject/src/main/java/edu/augustana/data/Card.java
+++ IbisGymProject/src/main/java/edu/augustana/data/Card.java
@@ -41,7 +41,7 @@ public class Card {
         this.title = selectedTitle;
         this.packFolder = selectedPackFolder;
         this.imagePath = selectedImagePath;
-        this.image = new Image("file:CardPack/DEMO1Pack/" + selectedImagePath);
+        this.image = new Image("file:CardPack/" + selectedPackFolder + "Pack/" + selectedImagePath);
         this.gender = selectedGender;
         this.modelSex = selectedModelSex;
         this.level = selectedLevel;
