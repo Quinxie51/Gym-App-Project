90c925e292165d69585b6bebc5c9d236a034a6be
Cami <camiflores21@augustana.edu>
2023-12-03 14:30:48 -0600

    added a undo redo funtionality and created/edited the proper classes

--- IbisGymProject/src/main/java/edu/augustana/ui/NewLessonPlanController.java
+++ IbisGymProject/src/main/java/edu/augustana/ui/NewLessonPlanController.java
@@ -471,6 +471,7 @@ public class NewLessonPlanController {
 
     private void restoreFromMemento(LessonPlanMemento memento) {
         MainApp.getCurrentCourse().setOneLessonPlan(new LessonPlan(memento.getLessonPlan()));
+        refreshLessonView(); // Update the UI after restoring
     }
 
     private void updateUndoRedoButtons() {
