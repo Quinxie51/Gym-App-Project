cdf47d1208406d546c64e546d789f8aac9b9efb6
Cody <codybyrd21@augustana.edu>
2023-12-03 15:19:21 -0600

    Made skelleton for print preview

--- IbisGymProject/src/main/java/edu/augustana/ui/PrintPreviewController.java
+++ IbisGymProject/src/main/java/edu/augustana/ui/PrintPreviewController.java
@@ -1,4 +1,19 @@
 package edu.augustana.ui;
 
+import edu.augustana.data.Card;
+import edu.augustana.data.LessonPlan;
+import javafx.fxml.FXML;
+
 public class PrintPreviewController {
+
+    private LessonPlan currentLessonPlan = MainApp.getCurrentCourse().getOneLessonPlan();
+
+    @FXML
+    private void initialize() {
+
+        for (Card card: currentLessonPlan.getCards()) {
+            //thingholding .add(card)
+        }
+
+    }
 }
