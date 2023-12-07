b37faa833736acb4495e5e611e62d7fc08a6f0f1
Cami <camiflores21@augustana.edu>
2023-12-07 13:06:36 -0600

    created zoom in and out funtionality

--- IbisGymProject/src/main/java/edu/augustana/ui/CardImageView.java
+++ IbisGymProject/src/main/java/edu/augustana/ui/CardImageView.java
@@ -2,12 +2,14 @@ package edu.augustana.ui;
 
 import edu.augustana.data.Card;
 import javafx.animation.ScaleTransition;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.util.Duration;
 
 public class CardImageView extends ImageView {
     private Card myCard;
+    private boolean isZoomed = false;
 
     public CardImageView(Image image, Card myCard) {
         super(image);
@@ -18,6 +20,18 @@ public class CardImageView extends ImageView {
                 handleCardActivation();
             }
         });
+
+
+        setOnMouseEntered(event -> {
+            handleCardZoom();
+        });
+
+        setOnMouseExited(event -> {
+            if (isZoomed) {
+                handleCardZoom(); // Zoom out when the mouse exits
+            }
+        });
+
     }
 
     // Method to set the active mode for a selected card
@@ -40,7 +54,25 @@ public class CardImageView extends ImageView {
     }
 
     // Method to handle card zoom in and out
+    private void handleCardZoom() {
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(200), this);
 
+        if (!isZoomed) {
+                // If the card is not zoomed in, zoom in
+            setActiveMode();
+            scaleTransition.setToX(2.0); // Adjust the scale factor as needed
+            scaleTransition.setToY(2.0);
+            isZoomed = true;
+        } else {
+                // If the card is already zoomed in, zoom out
+            resetStyles();
+            scaleTransition.setToX(1.0);
+            scaleTransition.setToY(1.0);
+            isZoomed = false;
+        }
+
+            scaleTransition.play();
+    }
 
     public Card getMyCard() {
         return myCard;
